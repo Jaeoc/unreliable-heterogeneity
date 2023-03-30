04ecde24550cf37427bd0ebc58b99256e1a400f5
Anton TiU <anton@olssoncollentine.com>
2023-03-30 16:53:09 +0200

    added truncation to normal distributions

--- code/supercomputer_simulator.r
+++ code/supercomputer_simulator.r
@@ -10,6 +10,16 @@
 library(parallel) #comes pre-installed with R
 #library(metafor) #loaded when setting up simulations
 
+rnorm_truncated <- function(n, mean, sd, lower_bound, upper_bound){
+    #Draw probababilities from a uniform distribution with the boundaries based on the relevant normal distribution
+    trunc_norm <- runif(n = n,
+                        min = pnorm(q = lower_bound, mean = mean, sd = sd),
+                        max = pnorm(q = upper_bound, mean = mean, sd = sd)
+    )
+    # Transform probabilities to x-values based on the normal distribution
+    qnorm(trunc_norm, mean = mean, sd = sd)
+}
+
 simulate_rma <- function(
     effect_type = c("r", "r_z"),
     reliability_distribution = c("uniform", "normal"),
@@ -24,15 +34,18 @@ simulate_rma <- function(
     steplength = 1, #these are for controlling the fisher algorithm in rma
     maxiter = 100){ #these are the default values in the function: https://www.metafor-project.org/doku.php/tips:convergence_problems_rma
 
-    # Draw rho from mean rho
-    rho <- rnorm(n = k, mean = mu, sd = sqrt(true_tau2))
-
+    # Draw rho from mean rho and compute sampling variances
     if(effect_type == "r") {
+
+        rho <- rnorm_truncated(n = k, mean = mu, sd = sqrt(true_tau2),
+                              lower_bound = -1, upper_bound = 1)
         sampling_var_rho  <- (1-rho^2)^2 / (sample_size -1)
 
     }else if(effect_type == "r_z") { #fisher's z
 
+        rho <- rnorm(n = k, mean = mu, sd = sqrt(true_tau2))
         sampling_var_rho  <- 1 / (sample_size - 3)
+
     }else{
         stop("specify effect type")
     }
@@ -42,18 +55,30 @@ simulate_rma <- function(
 
     #draw new reliabilities for each run to avoid some strange draw
     if(reliability_distribution == "uniform"){
-    reliabilities <- runif(k, min = reliability_min, max = reliability_max)
+
+        reliabilities <- runif(k, min = reliability_min, max = reliability_max)
+
     } else if(reliability_distribution == "normal"){
 
-        reliabilities <- rnorm(k, mean = reliability_mean, sd = reliability_sd)
-
-        while(max(reliabilities > 1)){
-        reliabilities <- rnorm(k, mean = reliability_mean, sd = reliability_sd)
-        }
-    } #note that in the second case we stop reliabilities from superceding 1, inducing some bias in the range of reliabilities
+        reliabilities <- rnorm_truncated(n = k,
+         mean = reliability_mean,
+         sd = reliability_sd,
+         lower_bound = 0,
+         upper_bound = 1)
+    } #we sample from a truncated normal distribution, inducing some bias in the range of reliabilities, at least on the upper limit
 
     #Compute observed r given measurement error
+    #If Fisher's z transform to r before adding measurement error
+    if(effect_type == "r_z"){
+        r_se <- tanh(r_se)
+    }
+
     r_se_me <- r_se*sqrt(reliabilities)^2
+
+    if(effect_type == "r_z"){
+        #Transform back to Fisher's z
+        r_se_me <- atanh(r_se_me)
+    }
 
     #compute observed sampling variance estimate given measurement error
     if(effect_type == "r") {
@@ -62,6 +87,7 @@ simulate_rma <- function(
         r_var_estimate  <- 1 / (sample_size - 3) #= sampling_var_rho
     }
 
+    # fit meta-analysis on observed values
     fit <- rma(yi = r_se_me, vi = r_var_estimate,
               control=list(stepadj=steplength, maxiter=maxiter))
 
@@ -71,14 +97,7 @@ simulate_rma <- function(
                tau2_p = fit$QEp)
 }
 
-r_to_z <- function(r){
-    z <- 0.5*log((1+r)/(1-r))
-    z
-}
-z_to_r <- function(z){
-    r <- (exp(2*z) - 1) / (exp(2*z) + 1)
-    r
-}
+
 #****************************************
 # Plot 1) effect size and variance in reliability estimates increase----
 #****************************************
@@ -87,13 +106,14 @@ z_to_r <- function(z){
 sample_size <- 150
 k <- 20
 true_tau2 <- 0 #Note, fixed effect model
-mu <- seq(from = 0, to = 1, by = 0.1)
+mu <- seq(from = 0, to = 0.9, by = 0.1)
+mu <- atanh(mu)
 
 #Based on Flake et al., average alpha was .79, SD = .13, range .17 - .87
 #Based on Sanchez-Meca, mean across 5 meta-analysis was 0.767 - 0.891 and SD ranged between 0.034 - 0.133
 reliability_mean <- 0.8
-reliability_sd <- seq(from = 0, to = 0.13, by = 0.01)
-reps <- 1e3
+reliability_sd <- seq(from = 0, to = 0.15, by = 0.05)
+reps <- 1e4
 
 cond <- expand.grid(sample_size = sample_size,
                     k = k,
@@ -133,7 +153,7 @@ clusterExport(cl, ls(.GlobalEnv)) #exported everything in environment to each no
                     1:reps, #looping over
                     function(iteration){ #anonymous function needed when using for replications
 
-                        simulate_rma(effect_type = "r",
+                        simulate_rma(effect_type = "r_z",
                                     reliability_distribution = "normal",
                                     k = cond_r$k,
                                     sample_size = cond_r$sample_size,
@@ -168,12 +188,12 @@ end - start
 
 sample_size <- 150
 k <- 20
-#true_tau2 <- 0.078 #corresponds to 95% I2 according to my simulations for Pearson's r and the above sample size and k
-true_tau2 <- 0.069 #corresponds to 90% I2 according to my simulations for Fisher's z and the above sample size and k
+true_tau2 <- 0.078 #corresponds to 95% I2 according to my simulations for Pearson's r and the above sample size and k
+#true_tau2 <- 0.069 #corresponds to 90% I2 according to my simulations for Fisher's z and the above sample size and k
 mu <- seq(from = 0, to = 0.6, by = 0.1)
 #Based on the 'upper median' (83.35% quantile) from Schäfer & Schwarz for non-preregistered studies.
-reliability_min <- seq(from = 0.1, to = 1, by = 0.1)
-reps <- 1e3
+reliability_min <- seq(from = 1, to = 1, by = 0.1)
+reps <- 1e2
 
 cond <- expand.grid(sample_size = sample_size,
                     k = k,
@@ -210,6 +230,7 @@ clusterExport(cl, ls(.GlobalEnv)) #exported everything in environment to each no
                     function(iteration){ #anonymous function needed when using for replications
 
                         simulate_rma(effect_type = "r",
+                                    reliability_distribution = "uniform",
                                     k = cond_r$k,
                                     sample_size = cond_r$sample_size,
                                     true_tau2 = cond_r$true_tau2,
@@ -241,10 +262,12 @@ end - start
 sample_size <- 150
 k <- 20
 true_tau2 <- seq(from = 0, to = 0.27, by = 0.01)
-#NB! Using Pearson's r as the effect size
 mu <- 0.24 #median effect size correlational studies (Michèles meta-meta-analysis)
+#Using Pearson's r as the effect size
+true_tau2 <- atanh(true_tau2) #transformed to fisher's z
+mu <- atanh(mu) #transformed to fisher's z
 reliability_min <- seq(from = 0.1, to = 1, by = 0.1)
-reps <- 1e3
+reps <- 1e4
 
 cond <- expand.grid(sample_size = sample_size,
                     k = k,
@@ -282,7 +305,7 @@ clusterExport(cl, ls(.GlobalEnv)) #exported everything in environment to each no
                     1:reps, #looping over
                     function(iteration){ #anonymous function needed when using for replications
 
-                        simulate_rma(effect_type = "r",
+                        simulate_rma(effect_type = "r_z",
                                     k = cond_r$k,
                                     sample_size = cond_r$sample_size,
                                     true_tau2 = cond_r$true_tau2,
@@ -312,16 +335,17 @@ end - start
 
 
 sample_size <- 150
-k <- 20
-true_tau2 <- 0.00078 #1/100 of 95% I2 tau2
-#NB! Using Pearson's r as the effect size
-mu <- seq(from = 0, to = 0.6, by = 0.1) #
+k <- c(5, 20)
+true_tau2 <- 0.069 #90% I2 in Fisher's z given above N and K
+#Using Pearson's r as the effect size
+mu <- seq(from = 0, to = 0.9, by = 0.1) #
+mu <- atanh(mu) #Fisher's z
 
 #Based on Flake et al., average alpha was .79, SD = .13, range .17 - .87
 #Based on Sanchez-Meca, mean across 5 meta-analysis was 0.767 - 0.891 and SD ranged between 0.034 - 0.133
-reliability_mean <- 0.8
-reliability_sd <- seq(from = 0, to = 0.13, by = 0.01)
-reps <- 1e3
+reliability_mean <- c(0.8, 0.9)
+reliability_sd <- seq(from = 0, to = 0.15, by = 0.05)
+reps <- 1e1
 
 cond <- expand.grid(sample_size = sample_size,
                     k = k,
@@ -361,7 +385,7 @@ clusterExport(cl, ls(.GlobalEnv)) #exported everything in environment to each no
                     1:reps, #looping over
                     function(iteration){ #anonymous function needed when using for replications
 
-                        simulate_rma(effect_type = "r",
+                        simulate_rma(effect_type = "r_z",
                                     reliability_distribution = "normal",
                                     k = cond_r$k,
                                     sample_size = cond_r$sample_size,
@@ -381,10 +405,10 @@ stopCluster(cl) # Shut down the nodes
 
 e <- lapply(out_list, function(x) do.call(rbind, x))
 e_means <- lapply(e, colMeans)
-names(e_means) <- c(paste0("mu = ", cond$mu, " & reliability_sd = ", cond$reliability_sd))
+names(e_means) <- c(paste0("mu = ", cond$mu, ";reliability_sd = ", cond$reliability_sd, ";k = ", cond$k, ";mean_rel = ", cond$reliability_mean))
 
-#saveRDS(e_means, "over_vs_underestimate.RDS")
-lapply(e_means, round, 3)
+saveRDS(e_means, "./data/test_over_vs_underestimate.RDS")
+#lapply(e_means, round, 3)
 
 end <- Sys.time()
 end - start
--- supplemental_data_max_tau2/simulate-given-k-n.r
+++ supplemental_data_max_tau2/simulate-given-k-n.r
@@ -10,15 +10,17 @@ simulate_rma_I2 <- function(effect_type = c("r", "r_z"), k, sample_size, true_ta
     #adjusted input and output compared to my primary function
     #Also, only for perfect reliability, i.e., uses sampling_var_rho to estimate meta-analysis
 
- # Draw rho from mean rho
-    rho <- rnorm(n = k, mean = mu, sd = sqrt(true_tau2))
+ # Draw rho from mean rho and compute sampling variances
 
     if(effect_type == "r") {
+        rho <- rnorm_truncated(n = k, mean = mu, sd = sqrt(true_tau2),
+                              lower_bound = -1, upper_bound = 1)
         sampling_var_rho  <- (1-rho^2)^2 / (sample_size -1)
 
     }else if(effect_type == "r_z") { #fisher's z
-
+        rho <- rnorm(n = k, mean = mu, sd = sqrt(true_tau2))
         sampling_var_rho  <- 1 / (sample_size - 3)
+
     }else{
         stop("specify effect type")
     }
@@ -44,10 +46,12 @@ library(parallel)
 #NB! sample size and K
 sample_size <- 150
 k <- 20
-true_tau2 <- seq(from = 0.05, to = 0.2, by = 0.001)
+#true_tau2 <- seq(from = 0.05, to = 0.2, by = 0.001)
 #Initial experimentation suggests 90% I2 is around 0.8 and 95% around 0.15
-#EDIT: for Fisher's z the above tau2 values are ok, for Pearson's r they need to be lower, about 0.03 to 0.1
-mu <- 0.3
+#EDIT: for Fisher's z the above tau2 values are ok, for Pearson's r they need to be lower, about 0.03 to 0.1. I have here used mu = 0.3/
+# To try to find low tau2 values for Pearson's r I update these values to  the below. I also set mu = 0.
+true_tau2 <- seq(from = 0, to = 0.05, by = 0.001)
+mu <- 0
 reps <- 1e3
 
 cond <- expand.grid(sample_size = sample_size,
