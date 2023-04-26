55a19c07ad16d61e75c483b749da6e0cab06530a
Anton TiU <anton@olssoncollentine.com>
2023-04-26 10:50:57 +0200

    added control over steplength to adjust for lower N not converging

--- code/supercomputer_simulator.r
+++ code/supercomputer_simulator.r
@@ -15,9 +15,10 @@ source("code/functions.r") #for rnorm_truncated and simulate_rma
 #****************************************
 # Conditions
 #****************************************
-sample_size <- 150
-k <- 20
-true_tau2 <- c(0, 0.002, 0.0035, 0.0055, 0.0085, 0.012, 0.0185, 0.031, 0.069)
+sample_size <- c(50, 100, 150, 200)
+k <- c(5, 20, 40, 200)
+true_tau <- c(0, 0.1, 0.15, 0.2)
+true_tau2 <- true_tau^2
 #Using Pearson's r as the effect size. Correspond to I2 20 - 90%.
 # Below corresponding values for k = 5
 # k <- 5
@@ -26,8 +27,8 @@ mu <- seq(from = 0, to = 0.6, by = 0.1) #
 
 #Based on Flake et al., average alpha was .79, SD = .13, range .17 - .87
 #Based on Sanchez-Meca, mean across 5 meta-analysis was 0.767 - 0.891 and SD ranged between 0.034 - 0.133
-reliability_mean <- c(0.6, 0.7, 0.8, 0.9, 1)
-reliability_sd <- seq(from = 0, to = 0.15, by = 0.05)
+reliability_mean <- c(0.6, 0.7, 0.8, 0.9)
+reliability_sd <- seq(from = 0.15, to = 0.15, by = 0.05)
 
 
 cond <- expand.grid(sample_size = sample_size,
@@ -41,16 +42,18 @@ cond <- expand.grid(sample_size = sample_size,
 remove_cond <- cond[["reliability_mean"]] == 1 & cond[["reliability_sd"]] > 0
 cond <- cond[!remove_cond,]
 
-#these below are just appendices to makes sure the function runs as expected
+#these below are higher level control input to the function
 cond$reliability_max <- cond$reliability_mean
 cond$reliability_min <- cond$reliability_mean
 cond$effect_type <- "r"
 cond$reliability_distribution <- "normal"
+cond$step_length  <-  0.5 #decrease from 1 to 0.5 to improve convergence for low N
+cond$maxiterations  <-  100 #default values are steplength = 1, and maxiter = 100
 
 #****************************************
 # Setup parallel simulation
 #****************************************
-reps <- 1e4
+reps <- 1e2
 out_list <- vector("list", length = nrow(cond))
 
 ncores <- detectCores()
@@ -83,7 +86,9 @@ task <- function(iteration){ #anonymous function needed when using for replicati
                     reliability_min = cond_r$reliability_min,
                     reliability_max = cond_r$reliability_max,
                     reliability_mean = cond_r$reliability_mean,
-                    reliability_sd = cond_r$reliability_sd)
+                    reliability_sd = cond_r$reliability_sd,
+                    steplength = cond_r$step_length,
+                    maxiter = cond_r$maxiterations)
     }
 
 
