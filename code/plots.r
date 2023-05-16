d9c68199ddf756726db65f2fdfc7b756f8c1957b
Anton TiU <anton@olssoncollentine.com>
2023-05-16 13:34:43 +0200

    updated code for z-r-hs plot

--- code/plots.r
+++ code/plots.r
@@ -53,7 +53,6 @@ library(ggplot2)
 
 #1) Create all Pearson's r plots
 
-
 dat_r <- readRDS("data/means_r_over_vs_underestimate.RDS")
 dat_z <- readRDS("data/means_z_over_vs_underestimate.RDS")
 dat_hs <- readRDS("data/means_r_HS_over_vs_underestimate.RDS")
@@ -66,18 +65,16 @@ dat <- data.table::rbindlist(dat, idcol = "mu")
 #create condition ids
 dat <- create_condition_cols(dat)
 
-
 # Add real truncated values
 trunc <- readRDS("data/truncated_tau.RDS")
-trunc$true_tau <- trunc$nominal_tau
-trunc$tau_hat <- trunc$tau
+trunc$true_tau <- trunc$tau
 
 #Prepping plot
 dat$true_tau2 <- as.numeric(dat$true_tau2)
 dat$mu <- as.numeric(dat$mu)
 
 dat$tau_hat <- sqrt(dat$tau2_hat)
-dat$true_tau <- sqrt(dat$true_tau2)
+dat$nominal_tau <- sqrt(dat$true_tau2)
 
 dat$mean_rel <- factor(dat$mean_rel)
 dat$k <- factor(dat$k, levels = c("5", "20", "40", "200"))
@@ -90,40 +87,66 @@ dat$N <- factor(dat$N, levels = c("50", "100", "150", "200"))
 dat_20_150 <- dat[dat$k == 20 & dat$N == 150,]
 
 # For fisher's z there is no truncation,
-# hack the trunc dataframe for the plot
+# create dataframe with all true heterogeneity levels
 trunc$effect_type  <- "Pearson's r"
-trunc <- rbind(trunc, trunc)
-trunc[29:56, "effect_type"] <- "Fisher's z"
+hs_true <- trunc
+hs_true$effect_type <- "Hunter & Schmidt" #effect type is r, but named this way for plotting
 
+z_true <- trunc
+z_true$effect_type <- "Fisher's z"
 dat_z <- dat_20_150[dat_20_150$effect_type == "r_z"]
-trunc$tau_hat[29:56] <- rep(unique(dat_z$true_tau), each = 7)
+z_tau <- rep(unique(dat_z$nominal_tau), each = 7) #4 tau-values across 4*7 conditions
+z_true$true_tau <- z_tau
+#NB! the z_true object has mu expressed in Pearson's r, not fisher z (this is useful for plotting later)
 
-dat_20_150$true_tau <- ifelse(dat_20_150$effect_type == "r_z",
-                              trunc(dat_20_150$true_tau *100) / 100,
-                              dat_20_150$true_tau)
+true_tau_line <- rbind(trunc, z_true, hs_true)
+true_tau_line$effect_type <- factor(true_tau_line$effect_type,
+                            levels = c("Fisher's z",
+                                        "Pearson's r",
+                                        "Hunter & Schmidt"))
+
+
+
+# truncate the fisher z nominal tau-values to match the pearson r ones
+#this is purely for plotting purposes. The true value can be seen from the black line in plot.
+dat_20_150$nominal_tau <- ifelse(dat_20_150$effect_type == "r_z",
+                              trunc(dat_20_150$nominal_tau *100) / 100,
+                              dat_20_150$nominal_tau)
 
 
 # What I've done in the above to lines of code is to
 # 1) put the 'truncated' true_tau for r_z to fisher z nominal value
 # 2) put the 'true tau' in the main data object to the nominal r value
 # This helps with the facet_grid to make the plot look nice
-# But changes the interpretation of the black line for fisher z
-# Does it? It is still the true heterogeneity
 
-# More plot prep
+# More plot polish
 dat_20_150$effect_type <- ifelse(dat_20_150$effect_type == "r_z",
                                 "Fisher's z",
                                 "Pearson's r")
+dat_20_150$effect_type <- ifelse(dat_20_150$effect_type == "Pearson's r" &
+                                 dat_20_150$method == "HS",
+                                "Hunter & Schmidt",
+                                dat_20_150$effect_type)
 
+dat_20_150$effect_type <- factor(dat_20_150$effect_type,
+                            levels = c("Fisher's z",
+                                        "Pearson's r",
+                                        "Hunter & Schmidt"))
+
+dat_20_150$mu <- ifelse(dat_20_150$effect_type == "Fisher's z",
+                        tanh(dat_20_150$mu),
+                        dat_20_150$mu)
 
 ggplot(dat_20_150, aes(x = mu, y = tau_hat)) +
-geom_line(aes(linetype = mean_reliability), show.legend = TRUE) +
-geom_line(data = trunc, linetype = 1) +
+geom_line(aes(linetype = mean_rel), show.legend = TRUE) +
 scale_linetype_manual(values = 2:5) +
 guides(linetype = guide_legend(reverse = TRUE)) +
+geom_line(aes(x = mu, y = true_tau), data = true_tau_line, linetype = 1) +
 expand_limits(y = 0) +
-facet_grid(effect_type~true_tau, scales = "free", switch = "y") +
+facet_grid(effect_type~nominal_tau, scales = "free", switch = "y") +
 theme_bw()
+
+ggsave("figures/z-r-hs-plot.png", width = 8.62, height = 9.93)
 
 #****************************************
 # Main plot 2 (r, N = 150, k variable, tau low)
