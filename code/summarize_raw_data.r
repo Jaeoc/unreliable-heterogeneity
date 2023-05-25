984c611edef076fc247284038c0a1419aa00b4c2
Anton TiU <anton@olssoncollentine.com>
2023-05-25 08:25:01 +0200

    code: simpliefied data cleaning code

--- code/summarize_raw_data.r
+++ code/summarize_raw_data.r
@@ -9,72 +9,11 @@
 
 source("./code/functions.r") # for create_condition_cols function
 
+
 #****************************************
-# Clean data raw data
+# Create dataset with means
 #****************************************
-
-#With r
-dat_r <- readRDS("data/raw_r_tau_0-0.2.RDS")
-
-#Turn list into dataframe using list-names as idcol
-dat_r <- data.table::rbindlist(dat_r, idcol = "mu")
-
-#create condition ids
-dat_r <- create_condition_cols(dat_r)
-
-saveRDS(dat_r, "data/cleaned_r_tau_0-0.2.RDS")
-
-
-# With z
-dat_z <- readRDS("data/raw_z_tau_0-0.2.RDS")
-
-#Turn list into dataframe using list-names as idcol
-dat_z <- data.table::rbindlist(dat_z, idcol = "mu")
-
-#create condition ids
-dat_z <- create_condition_cols(dat_z)
-
-saveRDS(dat_z, "data/cleaned_z_tau_0-0.2.RDS")
-
-e_means <- lapply(dat_z, colMeans)
-saveRDS(e_means, "data/means_z_tau_0-0.2.RDS")
-
 #e_medians <- lapply(e, function(x) apply(x, 2, median))
-
-# For HS method
-
-dat_hs <- readRDS("data/raw_r_HS_tau_0-0.2.RDS")
-
-#Turn list into dataframe using list-names as idcol
-dat_hs <- data.table::rbindlist(dat_hs, idcol = "mu")
-
-#create condition ids
-dat_hs <- create_condition_cols(dat_hs)
-
-saveRDS(dat_hs, "data/cleaned_hs_tau_0-0.2.RDS")
-
-e_means <- lapply(dat_hs, colMeans)
-saveRDS(e_means, "data/means_hs_tau_0-0.2.RDS")
-
-# For low tau
-
-dat_low <- readRDS("data/raw_r_tau_0.02-0.08.RDS")
-
-#Turn list into dataframe using list-names as idcol
-dat_low <- data.table::rbindlist(dat_low, idcol = "mu")
-
-#create condition ids
-dat_low <- create_condition_cols(dat_low)
-
-saveRDS(dat_low, "data/cleaned_r_tau_0.02-0.08.RDS")
-
-e_means <- lapply(dat_low, colMeans)
-saveRDS(e_means, "data/means_r_tau_0.02-0.08.RDS")
-
-
-#****************************************
-# Create means
-#****************************************
 
 #With r
 dat_r <- readRDS("data/raw_r_tau_0-0.2.RDS")
