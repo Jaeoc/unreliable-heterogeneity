9fdb2373273a9ebf9bb824d36b17d3ae8021664b
Anton TiU <anton@olssoncollentine.com>
2023-06-11 18:11:17 +0200

    updated main figures

--- code/plots.r
+++ code/plots.r
@@ -15,11 +15,9 @@ library(ggplot2) #for plotting
 # General plot prep
 #****************************************
 
-dat_r <- readRDS("data/means_r_over_vs_underestimate.RDS")
-dat_z <- readRDS("data/means_z_over_vs_underestimate.RDS")
-dat_hs <- readRDS("data/means_r_HS_over_vs_underestimate.RDS")
 
-dat <- c(dat_r, dat_z, dat_hs)
+dat <- read.csv("data/means_combined.csv")
+
 
 # Add real truncated values
 es <- seq(0, 0.6, 0.1)
@@ -33,21 +31,24 @@ trunc <- data.frame(mu = es,
 
 
 #Prepping plot
-dat$true_tau2 <- as.numeric(dat$true_tau2)
-dat$mu <- as.numeric(dat$mu)
-
 dat$tau_hat <- sqrt(dat$tau2_hat)
 dat$nominal_tau <- sqrt(dat$true_tau2)
 
-dat$mean_rel <- factor(dat$mean_rel)
 dat$k <- factor(dat$k, levels = c("5", "20", "40", "200"))
-dat$N <- factor(dat$N, levels = c("50", "100", "150", "200"))
-
+dat$N <- factor(dat$sample_size, levels = c("50", "100", "150", "200"))
 
 #****************************************
-# Main plot 1 (N = 150, k = 20, r, r_z, and HS)
+# Main manuscript plots
 #****************************************
-dat_20_150 <- dat[dat$k == 20 & dat$N == 150,]
+
+# Figure 1 (N = 150, k = 20, r, r_z, and HS)
+#****************************************
+dat_20_150 <- dat[dat$k == 20 & dat$N == 150 &
+                 dat$reliability_mean < 1 &
+                 dat$reliability_sd == 0.15,]
+
+dat_20_150$reliability_mean <- factor(dat_20_150$reliability_mean)
+
 
 # For fisher's z there is no truncation,
 # create dataframe with all true heterogeneity levels
@@ -57,7 +58,7 @@ hs_true$effect_type <- "Hunter & Schmidt" #effect type is r, but named this way 
 
 z_true <- trunc
 z_true$effect_type <- "Fisher's z"
-dat_z <- dat_20_150[dat_20_150$effect_type == "r_z"]
+dat_z <- dat_20_150[dat_20_150$effect_type == "r_z",]
 z_tau <- rep(unique(dat_z$nominal_tau), each = 7) #4 tau-values across 4*7 conditions
 z_true$true_tau <- z_tau
 #NB! the z_true object has mu expressed in Pearson's r, not fisher z (this is useful for plotting later)
@@ -80,7 +81,9 @@ dat_20_150$nominal_tau <- ifelse(dat_20_150$effect_type == "r_z",
 # What I've done in the above to lines of code is to
 # 1) put the 'truncated' true_tau for r_z to fisher z nominal value
 # 2) put the 'true tau' in the main data object to the nominal r value
-# This helps with the facet_grid to make the plot look nice
+# This helps with the facet_grid to make the plot look nice and helps us subset the tau-values of interest
+
+dat_20_150 <- dat_20_150[dat_20_150$nominal_tau %in% c(0, 0.1, 0.15, 0.2), ]
 
 # More plot polish
 dat_20_150$effect_type <- ifelse(dat_20_150$effect_type == "r_z",
@@ -101,78 +104,59 @@ dat_20_150$mu <- ifelse(dat_20_150$effect_type == "Fisher's z",
                         dat_20_150$mu)
 
 ggplot(dat_20_150, aes(x = mu, y = tau_hat)) +
-geom_line(aes(linetype = mean_rel), show.legend = TRUE) +
+geom_line(aes(linetype = reliability_mean), show.legend = TRUE) +
 scale_linetype_manual(values = 2:5) +
-guides(linetype = guide_legend(reverse = TRUE)) +
+guides(linetype = guide_legend(reverse = TRUE, title = "Mean Reliability")) +
 geom_line(aes(x = mu, y = true_tau), data = true_tau_line, linetype = 1) +
 expand_limits(y = 0) +
+ylab(expression("Between-studies standard deviation "~tau)) +
+xlab(expression("Average effect size "~mu)) +
 facet_grid(effect_type~nominal_tau, scales = "free", switch = "y") +
 theme_bw()
 
-ggsave("figures/z-r-hs-plot.png", width = 8.62, height = 9.93)
+#ggsave("figures/manuscript/z-r-hs-plot.png", width = 8.62, height = 9.93)
 
+
+# Figure 2 (r, N = 150, k variable, tau low)
 #****************************************
-# Main plot 2 (r, N = 150, k variable, tau low)
-#****************************************
-dat_r_low <- readRDS("data/means_r_tau_0.02-0.08.RDS")
-dat_r <- readRDS("data/means_r_tau_0-0.2.RDS")
 
-#The following line needs to be updated!
-dat_r_zero <- dat_r[grep("true_tau2 = 0;", names(dat_r))]
+dat_low <- dat[dat$N == 150 &
+               dat$method == "HV" &
+               dat$effect_type == "r" &
+               dat$nominal_tau %in% c(0.02, 0.04, 0.06, 0.08) &
+               dat$reliability_mean < 1 &
+               dat$reliability_sd == 0.15,]
 
-dat <- c(dat_r_zero, dat_r_low)
-dat <- lapply(dat, function(x) as.data.frame(as.list(x)))
-
-dat <- data.table::rbindlist(dat, idcol = "mu")
-
-#create condition ids
-dat <- create_condition_cols(dat)
-
-#Prepping plot
-dat$true_tau2 <- as.numeric(dat$true_tau2)
-dat$mu <- as.numeric(dat$mu)
-
-dat$tau_hat <- sqrt(dat$tau2_hat)
-dat$true_tau <- sqrt(dat$true_tau2)
-
-dat$mean_rel <- factor(dat$mean_rel)
-dat$k <- factor(dat$k, levels = c("5", "20", "40", "200"))
-dat$N <- factor(dat$N, levels = c("50", "100", "150", "200"))
-
-#****************************************
-# Truncated values
-
+# Truncated values (different from Figure 1 as tau is different)
 es <- seq(0, 0.6, 0.1)
-tau <- c(0, 0.02, 0.04, 0.06, 0.08)
+tau <- c(0.02, 0.04, 0.06, 0.08)
 
 tau_trunc <- lapply(tau, compute_var_truncated, mu = es)
 tau_trunc <- unlist(tau_trunc)
-trunc <- data.frame(mu = es,
+trunc_low <- data.frame(mu = es,
                     nominal_tau = rep(tau, each = length(es)),
                     true_tau = tau_trunc)
 
+trunc_low$true_tau <- trunc_low$nominal_tau
+trunc_low$tau_hat <- trunc_low$tau
 
-trunc$true_tau <- trunc$nominal_tau
-trunc$tau_hat <- trunc$tau
+dat_low$reliability_mean <- factor(dat_low$reliability_mean)
 
-#****************************************
-
-dat_n_150 <- dat[dat$N == 150 & dat$true_tau > 0,]
-trunc <- trunc[trunc$true_tau > 0,]
-
-dat_n_150 <- dat[dat$N == 150,]
 
 #Plot with fixed N and varying k, and small tau-values
-ggplot(dat_n_150, aes(x = mu, y = tau_hat)) +
-geom_line(aes(linetype = mean_reliability), show.legend = TRUE) +
-geom_line(data = trunc, linetype = 1) +
+ggplot(dat_low, aes(x = mu, y = tau_hat)) +
+geom_line(aes(linetype = reliability_mean), show.legend = TRUE) +
 scale_linetype_manual(values = 2:5) +
-guides(linetype = guide_legend(reverse = TRUE)) +
+geom_line(aes(x = mu, y = true_tau), data = trunc_low, linetype = 1) +
+guides(linetype = guide_legend(reverse = TRUE, title = "Mean Reliability")) +
 expand_limits(y = 0) +
-facet_grid(k~true_tau, scales = "free") +
+ylab(expression("Between-studies standard deviation "~tau)) +
+xlab(expression("Average effect size "~mu)) +
+facet_grid(k~nominal_tau, scales = "free") +
 theme_bw()
 
-ggsave("figures/r_tau_0.02-0.08.png", width = 8.62, height = 9.93)
+
+#ggsave("figures/manuscript/r_tau_0.02-0.08.png", width = 8.62, height = 9.93)
 
 
 #****************************************
@@ -187,7 +171,7 @@ dat_n_150 <- dat[dat$N == 150 & dat$effect_type == "r",]
 
 #Plot with fixed and and varying k
 ggplot(dat_n_150, aes(x = mu, y = tau_hat)) +
-geom_line(aes(linetype = mean_reliability), show.legend = TRUE) +
+geom_line(aes(linetype = reliability_meaniability), show.legend = TRUE) +
 geom_line(data = trunc, linetype = 1) +
 scale_linetype_manual(values = 2:5) +
 guides(linetype = guide_legend(reverse = TRUE)) +
@@ -201,7 +185,7 @@ dat_k_20 <- dat[dat$k == 20 & dat$effect_type == "r",]
 
 #Plot with fixed k and and varying N
 ggplot(dat_k_20, aes(x = mu, y = tau_hat)) +
-geom_line(aes(linetype = mean_reliability), show.legend = TRUE) +
+geom_line(aes(linetype = reliability_meaniability), show.legend = TRUE) +
 geom_line(data = trunc, linetype = 1) +
 scale_linetype_manual(values = 2:5) +
 guides(linetype = guide_legend(reverse = TRUE)) +
@@ -220,7 +204,7 @@ dat_n_150 <- dat[dat$N == 150 & dat$effect_type == "r_z",]
 
 #Plot with fixed and and varying k
 ggplot(dat_n_150, aes(x = mu, y = tau_hat)) +
-geom_line(aes(linetype = mean_reliability), show.legend = TRUE) +
+geom_line(aes(linetype = reliability_meaniability), show.legend = TRUE) +
 geom_line(data = trunc, linetype = 1) +
 scale_linetype_manual(values = 2:5) +
 guides(linetype = guide_legend(reverse = TRUE)) +
@@ -236,7 +220,7 @@ dat_k_20 <- dat[dat$k == 20 & dat$effect_type == "r_z",]
 
 #Plot with fixed k and and varying N
 ggplot(dat_k_20, aes(x = mu, y = tau_hat)) +
-geom_line(aes(linetype = mean_reliability), show.legend = TRUE) +
+geom_line(aes(linetype = reliability_meaniability), show.legend = TRUE) +
 geom_line(data = trunc, linetype = 1) +
 scale_linetype_manual(values = 2:5) +
 guides(linetype = guide_legend(reverse = TRUE)) +
