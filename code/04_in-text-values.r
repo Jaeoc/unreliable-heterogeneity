8a6302ef8b0fc985b1d61b74e145c6a94e268833
Anton TiU <anton@olssoncollentine.com>
2023-06-11 21:41:08 +0200

    updated in-text-value code

--- code/04_in-text-values.r
+++ code/04_in-text-values.r
@@ -12,17 +12,23 @@ source("./code/02_functions.r") # for create_condition_cols function
 #****************************************
 # [1] Result section
 #****************************************
-dat_r <- readRDS("data/means_r_tau_0-0.2.RDS")
+dat <- read.csv("data/means_combined.csv")
 
-dat_r$tau_hat <- sqrt(dat_r$tau2_hat)
-dat_r$true_tau <- sqrt(as.numeric(dat_r$true_tau2))
+dat$tau_hat <- sqrt(dat$tau2_hat)
+dat$true_tau <- sqrt(as.numeric(dat$true_tau2))
+dat$k <- factor(dat$k, levels = c("5", "20", "40", "200"))
+dat$N <- factor(dat$sample_size, levels = c("50", "100", "150", "200"))
 
-dat <- dat_r[dat_r$k == "20" & dat_r$N == "150",]
+dat <- dat[dat$k == "20" &
+           dat$N == "150" &
+           dat$reliability_sd == 0.15 &
+           dat$method == "HV" &
+           dat$effect_type == "r",]
 
 #*[1.1] bias when tau =0, R = 0.8 ----
 #****************************************
 
-dat_1.1 <- dat[dat$true_tau == 0 & dat$mean_rel == 0.6 ,]
+dat_1.1 <- dat[dat$true_tau == 0 & dat$reliability_mean == 0.8 ,]
 #mu = 0.2
 mu_0.2 <- dat_1.1$tau_hat[dat_1.1$mu == 0.2] - dat_1.1$true_tau[dat_1.1$mu == 0.2]
 mu_0.2 <- round(mu_0.2, 3)
@@ -35,30 +41,30 @@ bias_tau_zero <- data.frame(mu_0.2, mu_0.4)
 
 #*[1.2] bias when tau increasing, mu = 0.2, R = 0.8 ----
 #****************************************
-dat_1.2 <- dat[dat$mu == 0.2 & dat$mean_rel == 0.8,]
+dat_1.2 <- dat[dat$mu == 0.2 & dat$reliability_mean == 0.8,]
 #tau = 0.1
 tau_hat <- dat_1.2$tau_hat[dat_1.2$true_tau == 0.1]
 true_tau <- dat_1.2$true_tau[dat_1.2$true_tau == 0.1]
 bias_0.1 <-  tau_hat - true_tau
-percent_0.1 <- 100*(tau_hat / true_tau)
+prop_0.1 <- 1 - (tau_hat / true_tau)
 
-res_0.1 <- paste0(round(bias_0.1, 3), " (", round(percent_0.1, 0), "%)")
+res_0.1 <- paste0(round(bias_0.1, 3), " (", round(100*prop_0.1, 0), "%)")
 
 #tau = 0.15
 tau_hat <- dat_1.2$tau_hat[dat_1.2$true_tau == 0.15]
 true_tau <- dat_1.2$true_tau[dat_1.2$true_tau == 0.15]
 bias_0.15 <-  tau_hat - true_tau
-percent_0.15 <- 100*(tau_hat / true_tau)
+prop_0.15 <- 1 - (tau_hat / true_tau)
 
-res_0.15 <- paste0(round(bias_0.15, 3), " (", round(percent_0.15, 0), "%)")
+res_0.15 <- paste0(round(bias_0.15, 3), " (", round(100*prop_0.15, 0), "%)")
 
 #tau = 0.2
 tau_hat <- dat_1.2$tau_hat[dat_1.2$true_tau == 0.2]
 true_tau <- dat_1.2$true_tau[dat_1.2$true_tau == 0.2]
 bias_0.2 <-  tau_hat - true_tau
-percent_0.2 <- 100*(tau_hat / true_tau)
+prop_0.2 <- 1 - (tau_hat / true_tau)
 
-res_0.2 <- paste0(round(bias_0.2, 3), " (", round(percent_0.2, 0), "%)")
+res_0.2 <- paste0(round(bias_0.2, 3), " (", round(100*prop_0.2, 0), "%)")
 
 bias_increasing_tau <- list(tau_0.1 = res_0.1,
                             tau_0.15 = res_0.15,
@@ -67,17 +73,17 @@ bias_increasing_tau <- list(tau_0.1 = res_0.1,
 
 #*[1.3] bias when mu = 0.2 and tau => 0.1 ----
 #****************************************
-dat02 <- dat[dat$mu == 0.2 & dat$true_tau > 0,]
+dat02 <- dat[dat$mu == 0.2 & dat$true_tau >= 0.1,]
 dat02$prop <- dat02$tau_hat / dat02$true_tau
 
 dat02$underestimate <- 1 - dat02$prop
 
 #"heterogeneity can be expected to be underestimated by XX%"
 underestimate_min <- data.frame(dat02[which.min(dat02$underestimate),
- c("true_tau", "mean_rel", "underestimate")])
+ c("true_tau", "reliability_mean", "underestimate")])
 
 underestimate_max <- data.frame(dat02[which.max(dat02$underestimate),
- c("true_tau", "mean_rel", "underestimate")])
+ c("true_tau", "reliability_mean", "underestimate")])
 
 underestimate_mu02 <- list(underestimate_min = underestimate_min,
                            underestimate_max = underestimate_max)
